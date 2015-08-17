1076c7c6f6ac8c60f78d5c0f08f9ad665031975c
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-08-17 12:05:03 -0700

    Drivetrain instance variables and constants created

--- JFreyja/src/org/usfirst/frc/team8/robot/Ports.java
+++ JFreyja/src/org/usfirst/frc/team8/robot/Ports.java
@@ -1,5 +1,36 @@
 package org.usfirst.frc.team8.robot;
 
 public class Ports {
+	// Laptop USB
+	public static final int PORT_JOYSTICK_OPERATOR = 0;
+	public static final int PORT_JOYSTICK_DRIVE = 1;
+	public static final int PORT_JOYSTICK_TURN = 2;
 
+
+	// Pulse Width Modulation
+	public static final int PORT_DRIVETRAIN_TALON_LEFT_FRONT = 3;
+	public static final int PORT_DRIVETRAIN_TALON_LEFT_BACK = 2;
+	public static final int PORT_DRIVETRAIN_TALON_RIGHT_FRONT = 1;
+	public static final int PORT_DRIVETRAIN_TALON_RIGHT_BACK = 0;
+
+	public static final int PORT_LIFTER_VICTOR_1 = 4;
+	public static final int PORT_LIFTER_VICTOR_2 = 5;
+
+	public static final int PORT_ARM_SOLENOID_A = 6;
+	public static final int PORT_ARM_SOLENOID_B = 7;
+
+
+	// Digital Input/Output
+	public static final int PORT_DRIVETRAIN_ENCODER_LEFT_A = 0;
+	public static final int PORT_DRIVETRAIN_ENCODER_LEFT_B = 1;
+	public static final int PORT_DRIVETRAIN_ENCODER_RIGHT_A = 2;
+	public static final int PORT_DRIVETRAIN_ENCODER_RIGHT_B = 3;
+
+	public static final int PORT_LIFTER_ENCODER_A = 4;
+	public static final int PORT_LIFTER_ENCODER_B = 5;
+	public static final int PORT_LIFTER_HALL_EFFECT_TOP = 4;
+	public static final int PORT_LIFTER_HALL_EFFECT_BOTTOM = 5;
+
+	// Analog Input
+	public static final int PORT_GYROSCOPE = 1;
 }
--- JFreyja/src/org/usfirst/frc/team8/subsystems/Drivetrain.java
+++ JFreyja/src/org/usfirst/frc/team8/subsystems/Drivetrain.java
@@ -1,5 +1,72 @@
 package org.usfirst.frc.team8.subsystems;
 
-public class Drivetrain {
+import org.usfirst.frc.team8.robot.Ports;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Talon;
+
+public class Drivetrain {
+	/**
+	 * Keeps track of the State of the Drivetrain
+	 * for use in the update method
+	 *
+	 * TELEOP - Used for standard human controlled operation
+	 * AUTOMATED_DRIVE - Used when driving a set distance
+	 * AUTOMATED_ROTATE - Used when rotating a set angle
+	 * IDLE - Used when no motion is desired and no joystick controls have been made
+	 * BRAKING - Used to automatically stop the robot
+	 * STOPPED - Used for testing, no code running
+	 */
+	enum State{
+		IDLE,
+		TELEOP,
+		AUTOMATED_DRIVE,
+		AUTOMATED_ROTATE,
+		BRAKING,
+		STOPPED
+	};	
+	State state = State.IDLE;
+	
+	//PID constants
+	public static final double DRIVE_PROPORTIONAL = 0.12;
+	public static final double DRIVE_INTEGRAL = 0.0;
+	public static final double DRIVE_DERIVATIVE = 0.1;
+
+	 //Gyro PID constants
+	public static final double GYRO_PROPORTIONAL = 0.12;
+	public static final double GYRO_INTEGRAL = 0.0;
+	public static final double GYRO_DERIVATIVE = 0.1;
+
+	//Encoder constants
+	public static final double RIGHT_DPP = 0.1545595;
+	public static final double LEFT_DPP = 0.1577287;
+	public static final double ENCODER_INPUT_RANGE = 999;
+	public static final double ENCODER_DRIVE_OUTPUT_RANGE = 999;
+	public static final double ENCODER_GYRO_OUTPUT_RANGE = 999;
+	
+	//Talons
+	Talon leftTalon1 = new Talon(Ports.PORT_DRIVETRAIN_TALON_LEFT_FRONT);
+	Talon leftTalon2 = new Talon(Ports.PORT_DRIVETRAIN_TALON_LEFT_BACK);
+	Talon rightTalon1 = new Talon(Ports.PORT_DRIVETRAIN_TALON_RIGHT_FRONT);
+	Talon rightTalon2 = new Talon(Ports.PORT_DRIVETRAIN_TALON_RIGHT_BACK);
+
+	//Sensors
+	Gyro gyroscope = new Gyro(Ports.PORT_GYROSCOPE);
+	
+	//Encoders
+	Encoder leftEncoder = new Encoder(Ports.PORT_DRIVETRAIN_ENCODER_LEFT_A, Ports.PORT_DRIVETRAIN_ENCODER_LEFT_B, true);
+	Encoder rightEncoder = new Encoder(Ports.PORT_DRIVETRAIN_ENCODER_RIGHT_A, Ports.PORT_DRIVETRAIN_ENCODER_RIGHT_B, false);
+	
+	//PID Controllers
+	PIDController leftGyroscopeController1 = new PIDController(GYRO_PROPORTIONAL, GYRO_INTEGRAL, GYRO_DERIVATIVE, gyroscope, leftTalon1);
+	PIDController leftGyroscopeController2 = new PIDController(GYRO_PROPORTIONAL, GYRO_INTEGRAL, GYRO_DERIVATIVE, gyroscope, leftTalon2);
+	PIDController rightGyroscopeController1 = new PIDController(GYRO_PROPORTIONAL, GYRO_INTEGRAL, GYRO_DERIVATIVE, gyroscope, rightTalon1);
+	PIDController rightGyroscopeController2 = new PIDController(GYRO_PROPORTIONAL, GYRO_INTEGRAL, GYRO_DERIVATIVE, gyroscope, rightTalon2);
+	
+	PIDController leftDriveController1 = new PIDController(DRIVE_PROPORTIONAL, DRIVE_INTEGRAL, DRIVE_DERIVATIVE, leftEncoder, leftTalon1);
+	PIDController leftDriveController2 = new PIDController(DRIVE_PROPORTIONAL, DRIVE_INTEGRAL, DRIVE_DERIVATIVE, leftEncoder, leftTalon2);
+	PIDController rightDriveController1 = new PIDController(DRIVE_PROPORTIONAL, DRIVE_INTEGRAL, DRIVE_DERIVATIVE, leftEncoder, rightTalon1);
+	PIDController rightDriveController2 = new PIDController(DRIVE_PROPORTIONAL, DRIVE_INTEGRAL, DRIVE_DERIVATIVE, leftEncoder, rightTalon2);	
 }
