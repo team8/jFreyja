72995a4cc8b25468f06934f675e38a8ff8dacfae
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-08-18 10:50:09 -0700

    Added in subsystem abstract methods

--- JFreyja/src/org/usfirst/frc/team8/subsystems/Subsystem.java
+++ JFreyja/src/org/usfirst/frc/team8/subsystems/Subsystem.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc.team8.subsystems;
 
 public abstract class Subsystem {
-
+	/**
+	 * Initializes the subystem
+	 */
+	public abstract void init();
+	/**
+	 * Updates the subystem, to be called periodically
+	 */
+	public abstract void update();
+	/**
+	 * To disable the system when robot is disabled
+	 */
+	public abstract void disable();
+	/**
+	 * Makes the subsystem idle
+	 */
+	public abstract void idle();
+	public abstract boolean isIdle();
 }
