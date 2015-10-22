3f957478673632659e4e6d727af76f20cd8e1334
jdrumelhart <jdrumelhart@yahoo.com>
2015-10-22 10:18:48 -0700

    Created test suite

---
+++ JFreyja/src/jFreyjaTest/jFreyjaTestSuite.java
@@ -0,0 +1,15 @@
+package jFreyjaTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  DrivetrainTest.class,
+  LifterTest.class
+})
+
+public class jFreyjaTestSuite {
+  // the class remains empty,
+  // used only as a holder for the above annotations
+}
