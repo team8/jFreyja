37f30c0c0dafc208af4e184d0d2a42309063b396
Eric Liu <ericxliu@gmail.com>
2015-12-01 15:07:18 -0800

    Created Strongback project

---
+++ src/com/palyrobotics/robot/Robot.java
@@ -0,0 +1,30 @@
+/* Created Tue Dec 01 14:35:41 PST 2015 */
+package com.palyrobotics.robot;
+
+import org.strongback.Strongback;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void teleopInit() {
+        // Start Strongback functions ...
+        Strongback.start();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void disabledInit() {
+        // Tell Strongback that the robot is disabled so it can flush and kill commands.
+        Strongback.disable();
+    }
+
+}
---
+++ testsrc/com/palyrobotics/robot/TestRobot.java
@@ -0,0 +1,13 @@
+/* Created Tue Dec 01 14:35:41 PST 2015 */
+package com.palyrobotics.robot;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+    @Test
+    public void test() {
+        // do something here
+    }
+
+}
