f9312df2588f5b4f825ba5a60983a7ea7b3be9c7
Eric Liu <ericxliu@gmail.com>
2015-12-30 18:00:55 -0800

    removed statics

--- src/com/palyrobotics/robot/Drivetrain.java
+++ src/com/palyrobotics/robot/Drivetrain.java
@@ -3,21 +3,18 @@ package com.palyrobotics.robot;
 import org.strongback.components.Motor;
 import org.strongback.components.ui.FlightStick;
 import org.strongback.drive.TankDrive;
-import org.strongback.hardware.Hardware;
 
 public class Drivetrain extends TankDrive {
+	
 	private FlightStick stick;
 	
-	private static Motor leftBackMotor = Hardware.Motors.talon(0);
-	private static Motor leftFrontMotor = Hardware.Motors.talon(0);
-	private static Motor rightBackMotor = Hardware.Motors.talon(0);
-	private static Motor rightFrontMotor = Hardware.Motors.talon(0);
+	private Motor left;
+	private Motor right;
 	
-	private static Motor left = Motor.compose(leftBackMotor, leftFrontMotor);
-	private static Motor right = Motor.compose(rightBackMotor, rightFrontMotor);
-	
-	public Drivetrain(FlightStick stick) {
+	public Drivetrain(FlightStick stick, Motor left, Motor right) {
 		super(left, right);
+		this.left = left;
+		this.right = right;
 		this.stick = stick;
 	}
 	
--- src/com/palyrobotics/robot/Robot.java
+++ src/com/palyrobotics/robot/Robot.java
@@ -2,7 +2,9 @@
 package com.palyrobotics.robot;
 
 import org.strongback.Strongback;
+import org.strongback.components.Motor;
 import org.strongback.components.ui.FlightStick;
+import org.strongback.hardware.Hardware;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
@@ -12,9 +14,17 @@ public class Robot extends IterativeRobot {
 	
 	private FlightStick joystick;
 	
+	private Motor leftBackMotor = Hardware.Motors.talon(0);
+	private Motor leftFrontMotor = Hardware.Motors.talon(0);
+	private Motor rightBackMotor = Hardware.Motors.talon(0);
+	private Motor rightFrontMotor = Hardware.Motors.talon(0);
+	
+	private Motor left = Motor.compose(leftBackMotor, leftFrontMotor);
+	private Motor right = Motor.compose(rightBackMotor, rightFrontMotor);
+	
     @Override
     public void robotInit() {
-    	drivetrain = new Drivetrain(joystick);
+    	drivetrain = new Drivetrain(joystick, left, right);
     }
 
     @Override
