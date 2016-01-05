f7f117ac48f5c674046698be6c8e170e5fd410ac
Nihar Mitra <nihar.roy.mitra@gmail.com>
2016-01-04 16:06:51 -0800

    Change ports to constants in Constants.java

--- src/com/palyrobotics/robot/Constants.java
+++ src/com/palyrobotics/robot/Constants.java
@@ -4,5 +4,16 @@ public class Constants {
 	class Drivetrain {
 		public static final int PID_TOLERANCE = 1;		
 	}
-	
+	class Ports {
+		/** Pulse Width Modulation */
+		public static final int DRIVETRAIN_TALON_LEFT_FRONT = 3;
+		public static final int DRIVETRAIN_TALON_LEFT_BACK = 2;
+		public static final int DRIVETRAIN_TALON_RIGHT_FRONT = 1;
+		public static final int DRIVETRAIN_TALON_RIGHT_BACK = 0;
+		
+		/** Laptop USB */
+		public static final int JOYSTICK_OPERATOR = 0;
+		public static final int JOYSTICK_DRIVE = 1;
+		public static final int JOYSTICK_TURN = 2;
+	}
 }
--- src/com/palyrobotics/robot/Robot.java
+++ src/com/palyrobotics/robot/Robot.java
@@ -8,21 +8,23 @@ import org.strongback.hardware.Hardware;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
+import static com.palyrobotics.robot.Constants.Ports.*;
+
 public class Robot extends IterativeRobot {
 
 	//The drivetrain used
 	private Drivetrain drivetrain;
 	
 	//The joystick used
-	private FlightStick operatorStick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
-	private FlightStick driveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(1);
-	private FlightStick turnstick = Hardware.HumanInterfaceDevices.logitechAttack3D(2);
+	private FlightStick operatorStick = Hardware.HumanInterfaceDevices.logitechAttack3D(JOYSTICK_OPERATOR);
+	private FlightStick driveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(JOYSTICK_DRIVE);
+	private FlightStick turnstick = Hardware.HumanInterfaceDevices.logitechAttack3D(JOYSTICK_TURN);
 	
 	//The motors, these are used in the drivetrain
-	private Motor leftFrontMotor = Hardware.Motors.talon(3);
-	private Motor leftBackMotor = Hardware.Motors.talon(2);
-	private Motor rightFrontMotor = Hardware.Motors.talon(1);
-	private Motor rightBackMotor = Hardware.Motors.talon(0);
+	private Motor leftFrontMotor = Hardware.Motors.talon(DRIVETRAIN_TALON_LEFT_FRONT);
+	private Motor leftBackMotor = Hardware.Motors.talon(DRIVETRAIN_TALON_LEFT_BACK);
+	private Motor rightFrontMotor = Hardware.Motors.talon(DRIVETRAIN_TALON_RIGHT_FRONT);
+	private Motor rightBackMotor = Hardware.Motors.talon(DRIVETRAIN_TALON_RIGHT_BACK);
 	
 	//merging the motors on each side
 	private Motor left = Motor.compose(leftBackMotor, leftFrontMotor);
