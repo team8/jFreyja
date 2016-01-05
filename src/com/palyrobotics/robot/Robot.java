3e29afca71e429a6df43bcd9ddcd5b4b6384ed5f
Eric Liu <ericxliu@gmail.com>
2016-01-04 21:41:49 -0800

    added system for calling commands when stuff pressed

--- src/com/palyrobotics/robot/Robot.java
+++ src/com/palyrobotics/robot/Robot.java
@@ -2,9 +2,13 @@
 package com.palyrobotics.robot;
 
 import org.strongback.Strongback;
+import org.strongback.SwitchReactor;
+import org.strongback.components.AngleSensor;
 import org.strongback.components.Motor;
 import org.strongback.components.ui.FlightStick;
 import org.strongback.hardware.Hardware;
+
+import com.palyrobotics.commands.DriveDist;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
@@ -30,10 +34,19 @@ public class Robot extends IterativeRobot {
 	private Motor left = Motor.compose(leftBackMotor, leftFrontMotor);
 	private Motor right = Motor.compose(rightBackMotor, rightFrontMotor);
 	
+	//encoders. numbers are 1st port, 2nd port, dpp
+	private AngleSensor leftEncoder = Hardware.AngleSensors.encoder(0, 0, 0);
+	private AngleSensor rightEncoder = Hardware.AngleSensors.encoder(0, 0, 0);
+	
+	private SwitchReactor commandCaller;
+	
     @Override
     public void robotInit() {
     	//passes the motors to be used to the drivetrain, along with a joystick
     	drivetrain = new Drivetrain(turnstick, driveStick, left, right);
+    	
+    	//creates the syste that calls commands on button press
+    	commandCaller = Strongback.switchReactor();
     }
 
     @Override
@@ -44,8 +57,13 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
+    	
+    	//when operator trigger pressed, call drivedist with distance 10 and tolerance 1
+    	commandCaller.onTriggered(operatorStick.getTrigger(),()->Strongback.submit(new DriveDist(drivetrain, leftEncoder, rightEncoder, 10, 1)));
+    	
     	//constantly drives the robot according to joystick input
     	drivetrain.drive();
+    	
     }
 
     @Override
