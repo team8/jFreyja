16a05791a10ae0d5c7025d0df84ba01bc60d130a
jdrumelhart <jdrumelhart@yahoo.com>
2016-01-15 18:57:52 -0800

    Random edits

---
+++ src/test/RobotTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobotTest {
+
+	@Test
+	public void testRobotInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisabledInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeleopInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisabledPeriodic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeleopPeriodic() {
+		fail("Not yet implemented");
+	}
+
+}
