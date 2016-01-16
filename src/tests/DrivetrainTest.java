c11ff822ac58c3ba1ffb00aeaa745dd52863e74f
jdrumelhart <jdrumelhart@yahoo.com>
2016-01-15 17:09:42 -0800

    Created an exception test

--- src/tests/DrivetrainTest.java
+++ src/tests/DrivetrainTest.java
@@ -2,13 +2,28 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.strongback.components.*;
+import org.strongback.components.ui.*;
+
+import com.palyrobotics.robot.Drivetrain;
 
 public class DrivetrainTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
-	public void testDrivetrain() {
-		fail("Not yet implemented");
+	public void invalidDTMotors() {
+		FlightStick f1 = null;
+		FlightStick f2 = null;
+		Motor m1 = null;
+		Motor m2 = null;
+		thrown.expect(UnsatisfiedLinkError.class);
+		
+		Drivetrain d = new Drivetrain(f1,f2,m1,m2);
 	}
 
 	@Test
