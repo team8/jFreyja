264eafffc7816c4446e4d024de231a6dd3e08189
jdrumelhart <jdrumelhart@yahoo.com>
2016-01-15 16:35:22 -0800

    Created unit tests

---
+++ src/tests/DriveDistTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DriveDistTest {
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDriveDist() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/tests/DrivetrainTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DrivetrainTest {
+
+	@Test
+	public void testDrivetrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasArrived() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDriveDist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLeftMotor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRightMotor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTurnStick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDriveStick() {
+		fail("Not yet implemented");
+	}
+
+}
